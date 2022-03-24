d182ccab76510707a98ea523c7046c50972ee4ce
Darshan <darshanbastawade96@gmail.com>
2022-03-24 19:04:14 +0530

    Profile changes

---
+++ models/Profile.js
@@ -0,0 +1,127 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ProfileSchema = new Schema ({
+    //Used as id in Profiles.js 
+    user :{
+        type: Schema.Types.ObjectId,
+        //Ref used in populate
+        ref :'users'
+    },
+    handle:{
+        type:String,
+        required:true,
+        max:40
+    },
+    company:{
+        type:String
+    },
+    website:{
+        type:String
+    },
+    location:{
+        type:String
+    },
+    status:{
+        type:String,
+        required:true
+    },
+    skills:{
+        type:[String],
+        required:true
+    },
+    bio:{
+        type:String
+    },
+    githubusername:{
+        type:String
+    },
+    experince:[
+        {
+            title : {
+                type:String,
+                required:true
+            },
+            company : {
+                type:String,
+                required:true
+            },
+            location: {
+                type:String,
+               
+            },
+            from : {
+                type:Date,
+                required:true
+            },
+            to : {
+                type:Date
+            },
+            current:{
+                type:Boolean,
+                default:false
+            },
+            description : {
+                type:String
+            }
+            
+
+        }
+    ],
+    Education:[
+        {
+            school : {
+                type:String,
+                required:true
+            },
+            degree : {
+                type:String,
+                required:true
+            },
+            fieldofStudy: {
+                type:String,
+                required:true
+            },
+            from : {
+                type:Date,
+                required:true
+            },
+            to : {
+                type:Date
+            },
+            current:{
+                type:Boolean,
+                default:false
+            },
+            description : {
+                type:String
+            }
+            
+
+        }
+    ],
+    social:{
+        youtube:{
+            type:String
+        },
+        
+        twitter:{
+            type:String
+        },
+        instagram:{
+            type:String
+        },
+        facebook:{
+            type:String
+        },
+        linkdin:{
+            type:String
+        }
+    },
+    date :{
+        type:Date,
+        default:Date.now
+    }
+});
+
+module.exports = Profile = mongoose.model('profile',ProfileSchema);
